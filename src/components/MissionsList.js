ad0a784bb823b91350395e200bb97907c8061604
DavyKK <davykk04@gmail.com>
2022-08-23 21:34:32 +0300

    missionList created and styled

--- src/components/MissionsList.js
+++ src/components/MissionsList.js
@@ -1,12 +1,59 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMission, joinMission, leaveMission } from '../redux/Missions/MissionsReducer';
+import './mission.css';
 
-const MissionsList = () => (
-  <div className="missions-list">
-    <p>Missions 1</p>
-    <p>Missions 2</p>
-    <p>Missions 3</p>
-    <p>Missions 4</p>
-  </div>
-);
+const MissionsList = () => {
+  const missions = useSelector((state) => state.missions);
+  const dispatch = useDispatch();
+  const handleJoin = (id) => {
+    dispatch(joinMission(id));
+  };
+  const handleLeave = (id) => {
+    dispatch(leaveMission(id));
+  };
+
+  if (missions.length === 0) {
+    dispatch(fetchMission());
+  }
+  return (
+    <div>
+      <table className="mission-container">
+        <thead>
+          <tr className="tableHead">
+            <td className="missionTitle"><h1>Mision</h1></td>
+            <td className="mission-description"><h1>Description</h1></td>
+            <td className="missionStatus"><h1 className="status">Status</h1></td>
+            <td />
+          </tr>
+        </thead>
+        <tbody>
+          {
+          missions.map((mission) => (
+            <tr className="tableBody" key={mission.id}>
+              <td className="missionTitle"><h1>{mission.name}</h1></td>
+              <td className="mission-description"><p>{mission.description}</p></td>
+              <td className="missionStatus">
+                { mission.joined ? (
+                  <button className="member" type="button" onClick={() => handleLeave(mission.id)} disabled>Active Member</button>
+                ) : (
+                  <button className="notMember" type="button" onClick={() => handleJoin(mission.id)} disabled>NOT A MEMBER</button>
+                ) }
+              </td>
+              <td>
+                { mission.joined ? (
+                  <button className="leave" type="button" onClick={() => handleLeave(mission.id)}>Leave Mission</button>
+                ) : (
+                  <button className="join" type="button" onClick={() => handleJoin(mission.id)}>Join Mission</button>
+                ) }
+              </td>
+            </tr>
+          ))
+        }
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default MissionsList;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketsReducer from './Rockets/RocketsReducer';
+import missionsReducer from './Missions/MissionsReducer';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
-    categories: 'katana',
+    missions: missionsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
