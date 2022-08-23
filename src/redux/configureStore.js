17a283aef5a43772f5ed985535ba1f370032f5eb
Vitor Guedes <vgm_rox@hotmail.com>
2022-08-23 17:11:59 -0300

    solve conflicts

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import rocketsSlice from './Rockets/RocketsReducer';
+import missionsReducer from './Missions/MissionsReducer';
 
 const store = configureStore({
   reducer: {
     rockets: rocketsSlice,
-    categories: 'katana',
+    missions: missionsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
