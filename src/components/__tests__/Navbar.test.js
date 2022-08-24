44eeefc2f53f49d180710de2d25f358f0d5e5e3e
Vitor Guedes <vgm_rox@hotmail.com>
2022-08-24 13:05:16 -0300

    Add nav component testing

--- src/components/__tests__/Navbar.test.js
+++ src/components/__tests__/Navbar.test.js
@@ -1,9 +1,20 @@
 import { render } from '@testing-library/react';
 import Navbar from '../Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../../redux/configureStore';
+import React from 'react';
 
 describe('Navbar component', () => {
   test('Renders Navbar component', () => {
-    const tree = render(<Navbar />)
+    const tree = render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <Router>
+          <Navbar />
+        </Router>
+      </Provider>
+    </React.StrictMode>,)
     expect(tree).toMatchSnapshot()
   })
 });
